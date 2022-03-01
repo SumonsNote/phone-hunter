78cd655c667c22102c82d34f8f3c05cb263272e8
Sumonsnote <sohankhance@gmail.com>
2022-03-01 22:29:06 +0600

    only 20 phone show

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,28 @@
 const phoneFinder = () => {
-    document.getElementById("phone-container").innerHTML = '';
+  document.getElementById("phone-container").innerHTML = "";
   const inputValue = document.getElementById("phone-finder-input").value;
   document.getElementById("phone-finder-input").value = "";
-  if(!inputValue) {
-      document.getElementById('typeError').style.display = 'block'
-      document.getElementById('phone-detail-container').innerHTML = ''
-  }
-  else {
+  if (!inputValue) {
+    document.getElementById("typeError").style.display = "block";
+    document.getElementById("phone-detail-container").innerHTML = "";
+  } else {
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}
     `;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => showPhone(data.data));
-    document.getElementById('typeError').style.display = 'none'
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => showPhone(data.data));
+    document.getElementById("typeError").style.display = "none";
   }
 };
 
-
 const showPhone = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
-  for (const phone of phones) {
+  const resultPhone = phones.slice(0,20);
+  for (const phone of resultPhone) {
     const div = document.createElement("div");
-    div.classList.add('col-lg-4')
-    div.classList.add('col-md-6')
-    div.classList.add('col-12')
+    div.classList.add("col-lg-4");
+    div.classList.add("col-md-6");
+    div.classList.add("col-12");
     div.innerHTML = `
         <div class="card mt-5 container" style="width: 18rem;">
         <img width="200px" src="${phone.image}" class="card-img-top" alt="...">
@@ -39,30 +38,33 @@ const showPhone = (phones) => {
 };
 
 const phoneDetails = (phone) => {
-    const url = `https://openapi.programming-hero.com/api/phone/${phone}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => showPhoneDetails(data.data))
-}
+  const url = `https://openapi.programming-hero.com/api/phone/${phone}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showPhoneDetails(data.data));
+};
 
 const showPhoneDetails = (phoneDetail) => {
-    document.getElementById("phone-container").innerHTML = '';
-    const phoneDetailContainer = document.getElementById('phone-detail-container')
-    const div = document.createElement('div')
-    div.classList.add('p-5')
-    div.innerHTML = `
+  document.getElementById("phone-container").innerHTML = "";
+  const phoneDetailContainer = document.getElementById(
+    "phone-detail-container"
+  );
+  document.getElementById("phone-detail-container").innerHTML = "";
+  const div = document.createElement("div");
+  div.classList.add("p-5");
+  div.innerHTML = `
     <div class="card mt-5 container mx-auto" style="width: 18rem;">
     <img width="200px" src="${phoneDetail.image}" class="card-img-top" alt="...">
     <div class="card-body">
       <h3 class="card-title">${phoneDetail.name}</h3>
       <h5>${phoneDetail.brand}</h5>
-      <p>${phoneDetail.releaseDate}</p>
-      <p>${phoneDetail.mainFeatures.chipSet},${phoneDetail.mainFeatures.displaySize}</p>
-      <p>${phoneDetail.mainFeatures.storage}</p>
-      <p>${phoneDetail.mainFeatures.sensors}</p>
-      <p>${phoneDetail.others.Bluetooth},${phoneDetail.others.GPS},${phoneDetail.others.NFC},${phoneDetail.others.Radio},${phoneDetail.others.USB},${phoneDetail.others.WLAN}</p>
+      <p>${phoneDetail.releaseDate}</p><br>
+      <p><b>Chipset:</b> ${phoneDetail.mainFeatures.chipSet}<br>,<b>Size:</b> ${phoneDetail.mainFeatures.displaySize}</p><br>
+      <p><b>Memoery:</b> ${phoneDetail.mainFeatures.storage}</p><br>
+      <p><b>Sensors:</b> ${phoneDetail.mainFeatures.sensors}</p><br>
+      <p><b>Bluetooth:</b> ${phoneDetail.others.Bluetooth}<br><b>GPS:</b> ${phoneDetail.others.GPS}<br><b>NFC:</b> ${phoneDetail.others.NFC}<br><b>Radio:</b> ${phoneDetail.others.Radio}<br><b>USB:</b> ${phoneDetail.others.USB}<br><b>WLAN:</b> ${phoneDetail.others.WLAN}</p>
     </div>
   </div>
     `;
-    phoneDetailContainer.appendChild(div)
-}
+  phoneDetailContainer.appendChild(div);
+};
